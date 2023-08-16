5938f31eb5fddd450ad0a2f3f4bed11fbd4fd776
Martin Wittmann <107895957+martinwittmann90@users.noreply.github.com>
2023-08-15 22:16:01 -0300

    siguiendo

--- src/public/js/profile.js
+++ src/public/js/profile.js
@@ -30,22 +30,3 @@ logoutBtn.onclick = async () => {
     }, 2500);
   }
 };
-
-/*---------------CARROUSEL PRODUCTS HOME-----------------*/
-document.addEventListener('DOMContentLoaded', function () {
-  const carouselItems = document.querySelectorAll('.carousel-item');
-  let currentIndex = 0;
-
-  function showNextImage() {
-    carouselItems[currentIndex].classList.remove('active');
-    carouselItems[currentIndex].classList.add('hidden');
-
-    currentIndex = (currentIndex + 1) % carouselItems.length;
-
-    carouselItems[currentIndex].classList.remove('hidden');
-    carouselItems[currentIndex].classList.add('active');
-  }
-
-  // Mostrar la siguiente imagen cada 5 segundos
-  setInterval(showNextImage, 5000);
-});
